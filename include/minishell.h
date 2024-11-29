feb30f4337984a944f4b2e48ba40633ed6342130
Alan <aelaen@MacBook-Pro-de-Alan-2.local>
2024-11-29 11:00:50 +0100

    someadds

--- include/minishell.h
+++ include/minishell.h
@@ -2,12 +2,24 @@
  #define MINISHELL_H
 
 # include "libft/libft.h"
-# include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
-# include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <signal.h>
+# include <dirent.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
 
 #endif
