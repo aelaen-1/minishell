9e877de64db6c1976e7f9cf756fac407b4849313
aelaen-42 <aboukezi@bess-f3r3s6.clusters.42paris.fr>
2024-12-11 18:39:14 +0100

    gohome

--- tokenizer/lexer.c
+++ tokenizer/lexer.c
@@ -10,32 +10,32 @@ t_token *get_next_token(t_parse_context *context)
 {
     char *s;
 
-    int is_quoting = 0; // dquote
+    int is_dquoting = 0;
     s = context->input;
-    context->last_token = add_new_token(&context->array, context->input_len + 1); // context->array et context.array donnent la même chose ? 
+    context->last_token = add_new_token(&context->array, context->input_len + 1);
     while (s[context->position] && is_space(s[context->position]))
         context->position++;
     while(s[context->position])
     {
-        if (!is_quoting && is_space(s[context->position]))
+        if (!is_dquoting && is_space(s[context->position]))
             break ;
-        if (!is_quoting && s[context->position] == SIMPLE_QUOTE)
+        if (!is_dquoting && s[context->position] == SIMPLE_QUOTE)
         {
             char *start_quote = s + context->position + 1;
             char *next_quote = ft_strchr(start_quote, SIMPLE_QUOTE);
             if (next_quote != NULL)
             {
-                size_t  quote_len = next_quote - start_quote; // soustraction de pointeurs -> size_t
-                append_to_token(context->last_token, start_quote, quote_len); // context->last_token + j  ?
+                size_t  quote_len = next_quote - start_quote;
+                append_to_token(context->last_token, start_quote, quote_len);
                 context->position += quote_len + 1;
                 continue ; // ramène au début du while  sans passer par le reste des étapes
             }
         }
-        if (s[context->position] == DOUBLE_QUOTE)
+        if (s[context->position] == DOUBLE_QUOTE) // gestion differente pour le $expand
         {
-            if (is_quoting || ft_strchr(s + context->position + 1, DOUBLE_QUOTE))
+            if (is_dquoting || ft_strchr(s + context->position + 1, DOUBLE_QUOTE))
             {
-                is_quoting = !is_quoting;
+                is_dquoting = !is_dquoting;
                 context->position++;
                 continue ;
             }
@@ -46,18 +46,3 @@ t_token *get_next_token(t_parse_context *context)
     append_to_token(context->last_token, "\0", 1);
     return (context->last_token);
 }
-
-
-// else if (s[context->position] == DOUBLE_QUOTE)
-// {
-//     char *start_dquote = s + context->position + 1;
-//     char *next_dquote = ft_strchr(start_dquote, DOUBLE_QUOTE);
-//     if (next_dquote != NULL)
-//     {
-//         size_t  quote_len = next_dquote - start_dquote;
-//         ft_memcpy(context->last_token->value, start_dquote, quote_len); // renvoie void* , ft_memcpy(context->last_token, start_quote, quote_len);
-//         context->position += quote_len;
-//     }
-//     else
-//         return(*err_msg = UNCLOSED_DOUBLE_QUOTE, context->position);
-// }
--- tokenizer/parser.c
+++ tokenizer/parser.c
@@ -3,18 +3,19 @@
 void	print_tokens(char *input)
 {
     t_parse_context context;
-
-    init_token_array(&context.array);
+    t_token         *last_token;
+    if(!init_token_array(&context.array))
+    {
+        printf("array allocation failed\n");
+        return ;
+    }
     init_context(&context, input);
     while (context.position < context.input_len)
     {
-        t_token *last_token = get_next_token(&context);
-        // if (err_msg != NULL)
-        // {
-        //     write(STDERR_FILENO, err_msg, ft_strlen(err_msg));
-        //     break;
-        // }
+        last_token = get_next_token(&context);
+        printf("input : %s\n", context.input);
         printf("%s\n", last_token->value);
+        printf("waiting for next token :\n");
     }
     free_token_array(&context.array);
 }
