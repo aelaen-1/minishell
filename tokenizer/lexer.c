b4afb697e9e8af45d8439d61ec698b0995453cff
Alan <aelaen@mbp-de-alan-1.home>
2024-12-12 15:23:04 +0100

    new

--- tokenizer/lexer.c
+++ tokenizer/lexer.c
@@ -1,5 +1,6 @@
 #include "../include/minishell.h"
 
+// add src to the end of dest
 void    append_to_token(t_token *dest, char *src, size_t length)
 {
     size_t  i;
@@ -17,47 +18,27 @@ void    append_to_token(t_token *dest, char *src, size_t length)
     }
     dest->length += length;
 }
- 
+
+// saving chars in context->last_token until end of word
 t_token *get_next_token(t_parse_context *context)
 {
     char    *s;
     int is_dquoting;
 
-<<<<<<< HEAD
-    int is_dquoting = 0;
-=======
+
     is_dquoting = 0;
->>>>>>> 134b91e (bonsoir)
     s = context->input;
     context->last_token = add_new_token(&context->array, context->input_len + 1);
     while (s[context->position] && is_space(s[context->position]))
         context->position++;
     while(s[context->position])
     {
-<<<<<<< HEAD
-        if (!is_dquoting && is_space(s[context->position]))
+        if (is_space(s[context->position])) // retrait du !is_dquoting && 
             break ;
-        if (!is_dquoting && s[context->position] == SIMPLE_QUOTE)
-        {
-            char *start_quote = s + context->position + 1;
-            char *next_quote = ft_strchr(start_quote, SIMPLE_QUOTE);
-            if (next_quote != NULL)
-            {
-                size_t  quote_len = next_quote - start_quote;
-                append_to_token(context->last_token, start_quote, quote_len);
-                context->position += quote_len + 1;
-                continue ; // ramène au début du while  sans passer par le reste des étapes
-            }
-        }
-        if (s[context->position] == DOUBLE_QUOTE) // gestion differente pour le $expand
-=======
-        if (is_space(s[context->position])) // retrait du (!is_dquoting &&)
-            break ;
-        if (s[context->position] == SIMPLE_QUOTE) // retrait du (!is_dquoting &&)
+        if (s[context->position] == SIMPLE_QUOTE) // retrait du !is_dquoting && 
             if (handle_squote(s + context->position + 1, context))
-                continue;
-        if (s[context->position] == DOUBLE_QUOTE)
->>>>>>> 134b91e (bonsoir)
+                continue ; 
+        if (s[context->position] == DOUBLE_QUOTE) // gestion differente pour le $expand
         {
             if (is_dquoting || ft_strchr(s + context->position + 1, DOUBLE_QUOTE))
             {
--- tokenizer/parser.c
+++ tokenizer/parser.c
@@ -3,28 +3,18 @@
 void	print_tokens(char *input)
 {
     t_parse_context context;
-<<<<<<< HEAD
     t_token         *last_token;
+    
     if(!init_token_array(&context.array))
     {
         printf("array allocation failed\n");
         return ;
     }
-=======
-    t_token *last_token;
-
-    init_token_array(&context.array);
->>>>>>> 134b91e (bonsoir)
     init_context(&context, input);
     while (context.position < context.input_len)
     {
         last_token = get_next_token(&context);
-<<<<<<< HEAD
-        printf("input : %s\n", context.input);
-=======
->>>>>>> 134b91e (bonsoir)
         printf("%s\n", last_token->value);
-        printf("waiting for next token :\n");
     }
     destroy_tokens_array(&context.array);
 }
