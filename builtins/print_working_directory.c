2c71a277d2fb28407bf5aab664d561bae9509cef
lvastel <louisnavastel@gmail.com>
2024-12-13 16:53:58 +0100

    change

--- builtins/print_working_directory.c
+++ builtins/print_working_directory.c
@@ -6,13 +6,8 @@
 /*   By: lvastel <lvastel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 13:18:13 by lvastel           #+#    #+#             */
-/*   Updated: 2024/12/13 13:23:44 by lvastel          ###   ########.fr       */
+/*   Updated: 2024/12/13 16:52:15 by lvastel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-void    pwd(char *path)
-{
-    
-}
