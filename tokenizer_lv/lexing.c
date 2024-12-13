de23c1935bb6565b8c16f74a00e16d7584be1c49
lvastel <louisnavastel@gmail.com>
2024-12-13 12:08:02 +0100

    added pipe lexing

--- tokenizer_lv/lexing.c
+++ tokenizer_lv/lexing.c
@@ -1,63 +1,63 @@
-// #include "../include/minishell.h"
+#include "../include/minishell.h"
 
-// // add src to the end of dest
-// void    append_to_token(t_token *dest, char *src, size_t length)
-// {
-//     size_t  i;
-//     size_t  j;
+// add src to the end of dest
+void    append_to_token(t_token *dest, char *src, size_t length)
+{
+    size_t  i;
+    size_t  j;
 
-//     i = 0;
-//     while (dest->value[i])
-//         i++;
-//     j = 0;
-//     while (j < length)
-//     {
-//         dest->value[i] = src[j];
-//         i++;
-//         j++;
-//     }
-//     dest->length += length;
-// }
+    i = 0;
+    while (dest->value[i])
+        i++;
+    j = 0;
+    while (j < length)
+    {
+        dest->value[i] = src[j];
+        i++;
+        j++;
+    }
+    dest->length += length;
+}
 
-// // saving chars in context->last_token until end of word
-// t_token *get_next_token(t_parse_context *context)
-// {
-//     char    *s;
-//     int is_dquoting;
+// saving chars in context->last_token until end of word
+t_token *get_next_token(t_parse_context *context)
+{
+    char    *s;
+    int is_dquoting;
 
 
-//     is_dquoting = 0;
-//     s = context->input;
-//     context->last_token = add_new_token(&context->array, context->input_len + 1);
-//     while (s[context->position] && is_space(s[context->position]))
-//         context->position++;
-//     while(s[context->position])
-//     {
-//         if (is_space(s[context->position])) // retrait du !is_dquoting && 
-//             break ;
-//         if (s[context->position] == PIPE)
-//         {
-//             if (context->last_token->length > 0)
-//                 break;
-//             append_to_token(context->last_token, s + context->position, 1);
-//             context->position++;
-//             return (context->last_token);
-//         }
-//         if (s[context->position] == SIMPLE_QUOTE) // retrait du !is_dquoting && 
-//             if (handle_squote(s + context->position + 1, context))
-//                 continue ; 
-//         if (s[context->position] == DOUBLE_QUOTE) // gestion differente pour le $expand
-//         {
-//             if (is_dquoting || ft_strchr(s + context->position + 1, DOUBLE_QUOTE))
-//             {
-//                 is_dquoting = !is_dquoting;
-//                 context->position++;
-//                 continue ;
-//             }
-//         }
-//         append_to_token(context->last_token, s + context->position, 1);
-//         context->position++;
-//     }
-//     append_to_token(context->last_token, "\0", 1);
-//     return (context->last_token);
-// }
+    is_dquoting = 0;
+    s = context->input;
+    context->last_token = add_new_token(&context->array, context->input_len + 1);
+    while (s[context->position] && is_space(s[context->position]))
+        context->position++;
+    while(s[context->position])
+    {
+        if (is_space(s[context->position])) // retrait du !is_dquoting && 
+            break ;
+        if (s[context->position] == PIPE)
+        {
+            if (context->last_token->length > 0)
+                break;
+            append_to_token(context->last_token, s + context->position, 1);
+            context->position++;
+            return (context->last_token);
+        }
+        if (s[context->position] == SIMPLE_QUOTE) // retrait du !is_dquoting && 
+            if (handle_squote(s + context->position + 1, context))
+                continue ; 
+        if (s[context->position] == DOUBLE_QUOTE) // gestion differente pour le $expand
+        {
+            if (is_dquoting || ft_strchr(s + context->position + 1, DOUBLE_QUOTE))
+            {
+                is_dquoting = !is_dquoting;
+                context->position++;
+                continue ;
+            }
+        }
+        append_to_token(context->last_token, s + context->position, 1);
+        context->position++;
+    }
+    append_to_token(context->last_token, "\0", 1);
+    return (context->last_token);
+}
