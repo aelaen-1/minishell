4bee6a0b85977e57cbc28a76b2d1159f1bf6561b
lvastel <louisnavastel@gmail.com>
2024-12-13 12:57:19 +0100

    bzero token->value

--- tokenizer/token_array.c
+++ tokenizer/token_array.c
@@ -14,6 +14,7 @@ t_token    *add_new_token(t_token_array *array, size_t max_size)
     token->value = malloc(max_size);
     if (!token->value)
         return (free(token), NULL);
+    ft_bzero(token->value, max_size);
     if (array->count >= array->capacity)
     {
         t_token **new_tokens = malloc((array->capacity * 2) * sizeof(t_token *));
