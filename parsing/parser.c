c23f959bd22ace1e62795b50fb8c8bc5b7cee854
aelaen <alan.neghiz@gmail.com>
2024-12-18 16:54:05 +0100

    ne segfault plus mais ne compte pas les pipes

--- parsing/parser.c
+++ parsing/parser.c
@@ -9,7 +9,10 @@ static  size_t  count_pipes(t_token **start, t_token **end)
     while (start < end)
     {
         if ((*start)->type == TOKEN_PIPE)
+        {
+            printf("pipe found\n");
             i++;
+        }
         start++;
     }
     return (i);
@@ -31,28 +34,30 @@ t_token    **find_token(t_token **start, t_token **end, t_token_type token_type)
 // tant qu'on a pas atteint la fin du pipeline (A | B | C par ex => la fin est apres C, find_token renvoie NULL(char apres C))
 t_pipeline  *parse_pipeline(t_token **start, t_token **end)
 {
-    t_pipeline *pipeline = malloc(sizeof(t_pipeline));
+    t_pipeline *pipeline;
+    
+    pipeline = malloc(sizeof(t_pipeline));
     if (!pipeline)
         return (NULL);
     pipeline->cmd_count = count_pipes(start, end) + 1;
     pipeline->commands = malloc(sizeof(t_command *) * pipeline->cmd_count);
     if (!pipeline->commands)
         return (free(pipeline), NULL);
-    size_t  i = 0;
-    while (true)
+    size_t  i = 0; 
+    while (start < end && i < pipeline->cmd_count) // true
     {
         t_token **command_end = find_token(start, end, TOKEN_PIPE);
-        if (command_end != end)
+        pipeline->commands[i] = parse_command(start, command_end);
+        if (!pipeline->commands[i])
         {
-            pipeline->commands[i] = parse_command(start, command_end);
-            i++;
+            while (i > 0)
+                free(pipeline->commands[i--]);
+            free(pipeline->commands);
+            free(pipeline);
+            return (NULL);
         }
-        else
-        {
-            pipeline->commands[i] = parse_command(start, end);
-            break;
-        }
-        command_end += 1;
+        start = command_end + 1;
+        i++;
     }
     return (pipeline);
 }
@@ -60,17 +65,24 @@ t_pipeline  *parse_pipeline(t_token **start, t_token **end)
 t_command  *parse_command(t_token **start, t_token **end)
 {
     size_t  i;
+    size_t  argc;
     t_command   *command;
 
     i = 0;
+    argc = end - start;
     command = malloc(sizeof(t_command));
+    if (!command)
+        return (NULL);
+    command->argv = malloc(sizeof(char) * argc + 1);
+    if (!command->argv)
+        return (free(command), NULL);
     while(start < end)
     {
         command->argv[i] = (*start)->value;
         start++;
         i++;
     }
-    command->argv[i] = "\0";
+    command->argv[i] = NULL;
     return (command);
 }
 
