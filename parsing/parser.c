378918e56e4cf8db69d0729653d7e01d628eb7bd
aelaen-1 <alanbkzia@gmail.com>
2025-01-09 16:00:51 +0100

    Explication sur le fonctionnement de parse_program

--- parsing/parser.c
+++ parsing/parser.c
@@ -1,7 +1,8 @@
 #include "../include/minishell.h"
 
-// cree un programme constitue d'une pipeline et de ses commandes
-// s'il n'y a qu'une commande, program->pipeline est egale a la commande saisie
+// cree un programme constitue d'une pipeline et de ses commandes  
+// exemple : ls -a folder/ | wc -l | grep "1" : un pipeline, trois commandes
+// s'il n'y a qu'une commande, program->pipeline est egale a la commande saisie 
 // parse_pipeline() appelle parse_command() qui appelle parse_redir()
 t_program   *parse_program(t_token_array array)
 {
