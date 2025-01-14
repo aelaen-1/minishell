84b26b2210fa80cb85321d5e4969f094385e79aa
aloon <118343415+aelaen-1@users.noreply.github.com>
2025-01-14 16:41:10 +0100

    quote_removal done

--- lexing/quotes.c
+++ lexing/quotes.c
@@ -81,3 +81,73 @@ t_quote_type    *get_quote_state(char *s)
     }
     return (state);
 }
+
+static void     remove_str_quotes(char *str)
+{
+    t_quote_type    *quote_state;
+    t_quote_type    current;
+    size_t  i;
+    size_t  j;
+
+    current = QUOTE_NONE;
+    quote_state = get_quote_state(str);
+    i = 0;
+    j = 0;
+    while (str[i])
+    {
+        if (quote_state[i] != current)
+        {
+            current = quote_state[i];
+            i++;
+            if (!str[i])
+                break ;
+        }
+        else
+            str[j++] = str[i++];
+    }
+    str[j] = '\0';
+    free(quote_state);
+}
+
+void    remove_quotes(t_command *cmd)
+{
+    size_t  i;
+
+    i = 0;
+    while (cmd->argv[i])
+    {
+        remove_str_quotes(cmd->argv[i]);
+        i++;
+    }
+}
+
+// enleve les quotes trouvees une fois les expansions faites
+// exemple dans shell : input : echo $"PATH" ==> "PATH" ==> output = PATH  : le shell fait d'abord l'expansion, ici
+// pas de variable d'environnement juste apres le $ donc il saute le $ et passe a "PATH"
+// void    remove_str_quotes(char *str)
+// {
+//     size_t  i;
+//     t_quote_type    *state;
+//     t_quote_type    current;
+
+//     i = 0;
+//     current = QUOTE_NONE;
+//     state = get_quote_state(str);
+//     while (str[i])
+//     {
+//         if (state[i] != current)
+//         {
+//             current = state[i];
+//             char *opening = str + i;
+//             char *closing = ft_strchr(opening + 1, SIMPLE_QUOTE);
+//             if (closing)
+//             {
+//                 ft_memmove(closing, closing + 1, ft_strlen(closing));
+//                 ft_memmove(opening, opening + 1, ft_strlen(opening));
+//                 i += closing - opening;
+//             }
+//         }
+//         else
+//             i++;
+//     }
+// }
