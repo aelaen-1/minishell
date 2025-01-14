5e43459dc3c0ae08169f256e3871e639e3a4c38f
aloon <118343415+aelaen-1@users.noreply.github.com>
2025-01-14 16:48:29 +0100

    clean up main

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "include/minishell.h"
-//REPL
-//./minishell --pp(print_program)
+
+//REPL 
 int main (int ac, char **av , char **env)
 {
     (void)ac;
@@ -9,25 +9,9 @@ int main (int ac, char **av , char **env)
     t_env_node *envp;
 
     envp = init_env(env);
-    if (ac == 2 && !strcmp(av[1], "--pp"))
-    {
-        while (1)
-        {
-        char *input = readline("minishell % ");
-        t_token_array tokens = tokenize_input(input);
-        t_program *program = parse_program(tokens, envp);
-        print_program_to_dot(program);
-        destroy_tokens_array(&tokens);
-        }
-        return 0;
-    }
     while(1)
     {
         char *input = readline("minishell % ");
-        // printf("%s\n", input);
-        // t_quote_type *quoting = get_quote_state(input);
-        // for (size_t i =0; i < ft_strlen(input); i++)
-        //     printf("%d", quoting[i]);
 		if (*input)
 			add_history(input);
 		t_token_array tokens = tokenize_input(input);
