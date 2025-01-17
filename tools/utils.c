64c67a7e8051f78a0d9960737ee15f8cd69a8812
GuillaumeLa <pro.glabad@gmail.com>
2025-01-17 02:36:06 +0100

    norminette v2 | manque plus que les 3 dernieres fonctions de plus de 25 lignes et trop de variables + les commentaires

--- tools/utils.c
+++ tools/utils.c
@@ -1,41 +1,45 @@
 #include "../include/minishell.h"
 
-int is_space(char c)
+int	is_space(char c)
 {
-    if (c == 32 || (c >= 9 && c <= 13))
-        return (1);
-    return (0);
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
 }
 
-size_t  eat_spaces(t_lex_context *context)
+size_t	eat_spaces(t_lex_context *context)
 {
-    size_t  spaces;
+	size_t	spaces;
 
-    spaces = 0;
-    while(is_space(context->input[context->position]))
-    {
-        context->position++;
-        spaces++;
-    }
-    return (spaces);
+	spaces = 0;
+	while (is_space(context->input[context->position]))
+	{
+		context->position++;
+		spaces++;
+	}
+	return (spaces);
 }
 
-int ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-    size_t i = 0;
-    while (s1[i] && s2[i] && s1[i] == s2[i])
-        i++;
-    return (s1[i] - s2[i]);
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
 
-void free_split(char **s)
+void	free_split(char **s)
 {
-    int i = 0;
-    while (s[i])
-    {
-        free(s[i]);
-        i++;
-    }
-    free(s);
-    s = NULL;
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s);
+	s = NULL;
 }
