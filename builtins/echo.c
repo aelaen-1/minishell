32018b7e2e5ba2ff397fa16984cfed580dc0f8ef
aloon <118343415+aelaen-1@users.noreply.github.com>
2025-01-21 22:01:53 +0100

    working echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -1,19 +1,38 @@
 #include "../include/minishell.h"
 
+
+static void	skip_n(t_command *command, size_t *i, int *n_option)
+{
+	while (command->argv[*i])
+	{
+		if (command->argv[*i][0] == '-' && command->argv[*i][1] == 'n')
+		{
+			size_t j = 2;
+			while (command->argv[*i][j] == 'n')
+				j++;
+			if (command->argv[*i][j] == '\0')
+			{
+				(*n_option)++;
+				(*i)++;
+			}
+			else
+				break ;
+		}
+		else
+			break ;
+	}
+}
+
 int	builtin_echo(t_command *command)
 {
-	int	i;
+	size_t	i;
 	int	n_option;
 
 	i = 1;
 	n_option = 0;
 	if (!command->argv[i])
 		return (ft_putchar_fd('\n', command->fds[1]), 0);
-	while (command->argv[i] && !ft_strcmp("-n", command->argv[i]))
-	{
-		n_option = 1;
-		i++;
-	}
+	skip_n(command, &i, &n_option);
 	while (command->argv[i])
 	{
 		ft_putstr_fd(command->argv[i], command->fds[1]);
