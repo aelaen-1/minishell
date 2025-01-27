d5f0a789ab8cc8de8002812484cbfd35de3584a5
aloon <118343415+aelaen-1@users.noreply.github.com>
2025-01-27 11:21:32 +0100

    refacto

--- main.c
+++ main.c
@@ -2,49 +2,63 @@
 
 extern int		g_sig;
 
-int	main(int ac, char **av, char **env)
-{
-	t_program		*prg;
-	t_env_node		*envp;
-	char			*input;
-	t_token_array	tokens;
-	t_context	context;
 
-	(void)ac;
-	(void)av;
-	envp = init_env(env);
-	context.envp = envp;
-	context.last_cmd_status = 0;
+static void	init_program_context(t_context *context, t_env_node *envp)
+{
+	context->envp = envp;
+	context->last_cmd_status = 0;
 	signal_handler();
-	prg = NULL;
-	int should_loop = 1;
+}
+
+static	void	shell_repl_loop(t_context *context)
+{
+	char	*input;
+	t_token_array	tokens;
+	t_program	*program;
+	int		should_loop;
+
+	should_loop = 1;
 	while (should_loop)
 	{
 		g_sig = 0;
 		input = readline("minishell % ");
 		if (g_sig)
-			context.last_cmd_status = 130;
+			context->last_cmd_status = 130;
 		if (!input)
 			break ;
-		if (!check_input(input, &context))
+		if (!check_input(input, context))
 		{
 			free(input);
 			continue ;
 		}
 		tokens = tokenize_input(input);
-		prg = parse_program(tokens);
-		if(prg)
+		program = parse_program(tokens);
+		if(program)
 		{
-			if (execute_program(prg, &context) == -1)
+			if (execute_program(program, context) == -1)
 				should_loop = 0;
-			free_program(prg);
+			free_program(program);
 		}
 		destroy_tokens_array(&tokens);
 		free(input);
 		if (g_sig)
-			context.last_cmd_status = 130;
+			context->last_cmd_status = 130;
 	}
-	rl_clear_history();
-	free_env_node(envp);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	t_env_node		*shell_env;
+	t_context	context;
+
+	(void)ac;
+	(void)av;
+	shell_env = init_env(env);
+	if (!shell_env)
+		return (perror("Failed to initialize environment"), 1);
+	init_program_context(&context, shell_env);
+	shell_repl_loop(&context);
+	// rl_clear_history();
+	free_env_node(shell_env);
 	return (0);
 }
