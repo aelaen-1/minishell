b777f6ebec8ce7796b74d7b139a30260b2612595
aelaen-1 <alanbkzia@gmail.com>
2025-01-27 23:10:50 +0100

    free

--- parsing/parser.c
+++ parsing/parser.c
@@ -6,7 +6,7 @@ t_program	*parse_program(t_token_array array)
 
 	program = malloc(sizeof(t_program));
 	if(!program)
-		return (0);
+		return (NULL);
 	program->pipeline = parse_pipeline(array.tokens,
 			array.tokens + array.count);
 	if(!program->pipeline)
