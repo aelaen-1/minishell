aa5c46de234c26a4c16d045b34b9a0770a564606
aelaen-1 <alanbkzia@gmail.com>
2025-01-29 13:57:21 +0100

    parse_program norm

--- parsing/parser.c
+++ parsing/parser.c
@@ -7,11 +7,11 @@ t_program	*parse_program(t_token_array array)
 	if (array.count == 0)
 		return (NULL);
 	program = malloc(sizeof(t_program));
-	if(!program)
+	if (!program)
 		return (NULL);
 	program->pipeline = parse_pipeline(array.tokens,
 			array.tokens + array.count);
-	if(!program->pipeline)
-		return(free(program), NULL);
+	if (!program->pipeline)
+		return (free(program), NULL);
 	return (program);
 }
