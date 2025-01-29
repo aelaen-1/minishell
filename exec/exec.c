b61c5332df02f19144ebf58dd62862986c3a7cdd
aelaen-1 <alanbkzia@gmail.com>
2025-01-30 00:01:01 +0100

    exec refacto

--- exec/exec.c
+++ exec/exec.c
@@ -17,7 +17,7 @@ static void	link_pipeline(t_pipeline *pipeline)
 	}
 }
 
-static char	*get_path(t_command *cmd, t_env_node *envp, int	*should_free_path)
+static char	*get_path(t_command *cmd, t_env_node *envp)
 {
 	char	**full_path;
 	size_t	i;
@@ -28,7 +28,7 @@ static char	*get_path(t_command *cmd, t_env_node *envp, int	*should_free_path)
 
 	i = 0;
 	if(access(cmd->argv[0], F_OK | X_OK) == 0)
-		return (*should_free_path = 0, cmd->argv[0]);
+		return (ft_strdup(cmd->argv[0]));
 	path_env_value = get_env_value("PATH", envp);
 	if (!path_env_value)
 		return (ft_putstr_fd("minishell: PATH not set\n", 2), NULL);
@@ -46,12 +46,11 @@ static char	*get_path(t_command *cmd, t_env_node *envp, int	*should_free_path)
 	return (free(path_env_value), free_split(full_path), NULL);
 }
 
-static	int	fork_error(char **env, char *path, int *should_free_path)
+static	int	fork_error(char **env, char *path)
 {
 	ft_putstr_fd("Fork error\n", 2);
 	free_split(env);
-	if (*should_free_path == 1)
-		free(path);
+	free(path);
 	return (-1);
 }
 
@@ -62,14 +61,43 @@ static	int	exec_builtin(t_command *cmd, t_context *context)
 	return (1);
 }
 
+int	child_process(t_command *cmd, char *path, char **env)
+{
+	int	res;
+
+	res = 0;
+	dup2(cmd->fds[0], 0);
+	dup2(cmd->fds[1], 1);
+	close_command_fds(cmd);
+	res = execve(path, cmd->argv, env);
+	return (res);
+
+}
+
+static int do_fork(t_command *cmd, int *pid, char *path, t_context *context)
+{
+	char **env;
+	int	res;
+
+	res = 0;
+	env = lst_to_char(context->envp);
+	*pid = fork();
+	if (*pid == -1)
+		return (fork_error(env, path));
+    if (*pid == 0)
+		res = child_process(cmd, path, env);
+	else
+		close_command_fds(cmd);
+	free_split(env);
+	return (res);
+}
+
 static int  exec_cmd(t_command *cmd, int *pid, t_context *context)
 {
-	int should_free_path;
 	char *path;
-	char **env;
+	int	res;
 	
-	env = NULL;
-	should_free_path = 1;
+	res = 0;
     if (!cmd || !cmd->argv[0])
         return (0);
     expand_command(cmd, context);
@@ -79,39 +107,12 @@ static int  exec_cmd(t_command *cmd, int *pid, t_context *context)
 		return (0);
     if (is_builtin(cmd))
 		return (exec_builtin(cmd, context));
-	path = get_path(cmd, context->envp, &should_free_path);
-	if (!path)
-		context->last_cmd_status = handle_exec_error(cmd->argv[0], context);
-	else
-		context->last_cmd_status = handle_exec_error(path, context);
+	path = get_path(cmd, context->envp);
+	context->last_cmd_status = handle_exec_error(path ? path : cmd->argv[0], context);
 	if (context->last_cmd_status)
-	{
-		if (should_free_path == 1)
-			free(path);
-		return (context->last_cmd_status);
-	}
-	env = lst_to_char(context->envp);
-    *pid = fork();
-	if (*pid == -1)
-		return (fork_error(env, path, &should_free_path));
-    if (*pid == 0)
-    {
-        dup2(cmd->fds[0], 0);
-        dup2(cmd->fds[1], 1);
-        close_command_fds(cmd);
-        if (execve(path, cmd->argv, env) == -1)
-		{
-			if (should_free_path == 1)
-				free(path);
-			return (free_split(env), -1);
-		}
-    }
-	else
-		close_command_fds(cmd);
-	free_split(env);
-	if (should_free_path == 1)
-		free(path);
-	return (0);
+		return (free(path), context->last_cmd_status);
+	res = do_fork(cmd, pid, path, context);
+	return (free(path), res);
 }
 
 int	execute_program(t_program *program, t_context *context)
